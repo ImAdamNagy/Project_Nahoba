8240e31a0d574da32122ea9749f4e66dc61e3441
batoriandras <batori.andras@gmail.com>
2023-03-20 09:04:33 +0100

    register page

---
+++ NahobaVue/nahoba/src/components/layouts/Register.vue
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<template>
+<div class="container">
+    <div class="row py-5">
+        <div class="col-4 ms-auto rounded-start rounded-3">
+            <div class="bg-danger" id="formdiv">
+                <form action="">
+                    <input type="email" name="email" id="email" placeholder="email" class="form-control">
+                    <div id="name">
+                        <input type="text" name="firstname" id="firstname" placeholder="First Name" class="form-control me-2">
+                        <input type="text" name="lastname" id="lastname" placeholder="Last Name" class="form-control ">
+                    </div>
+                    <input type="tel" name="tel" id="tel" placeholder="tel" class="form-control">
+                    <input type="text" name="address" id="address" placeholder="address" class="form-control">
+                    <input type="text" name="username" id="username" placeholder="username" class="form-control">
+                    <input type="password" name="password" id="password" placeholder="password" class="form-control">
+                    <input type="password" name="passwordconf" id="passwordconf" placeholder="confirm password" class="form-control">
+                    <input type="submit" value="Register" class="btn btn-success mx-auto">
+                </form>
+            </div>
+        </div>
+        <div class="col-5 bg-warning me-auto rounded-end rounded-3">
+            kép
+
+        </div>   
+    </div>
+</div>
+</template>
+
+<style scoped>
+    #name{
+        display: flex;
+        flex-direction: row;
+    }
+    input{
+        margin-top: 7px;
+    }
+    #formdiv{
+        padding: 10px;
+    }
+
+</style>
--- NahobaVue/nahoba/src/router/index.js
+++ NahobaVue/nahoba/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPageView from "../views/MainPageView.vue"
+import RegisterPageView from "../views/RegisterPageView.vue"
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ export const router = createRouter({
       path: '/',
       name: 'MainPage',
       component: MainPageView,
+    },
+    {
+      path: '/register',
+      name: 'RegisterPage',
+      component: RegisterPageView,
     },
   ]
 });
