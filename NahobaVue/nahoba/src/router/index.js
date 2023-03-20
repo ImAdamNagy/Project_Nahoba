73b6c5076311153237efe7f6722fd72e77411a41
KriszRaven <hollokg@gmail.com>
2023-03-20 09:43:47 +0100

    merge conflict fixed

--- NahobaVue/nahoba/src/router/index.js
+++ NahobaVue/nahoba/src/router/index.js
@@ -1,11 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPageView from "../views/MainPageView.vue"
-<<<<<<< HEAD
 import RegisterPageView from "../views/RegisterPageView.vue"
-=======
 import CreateProductPageView from "../views/CreateProductPageView.vue"
 import LoginPageView from "../views/LoginPageView.vue"
->>>>>>> 5d848de14d86f39d589fed72cef192d54c6cd5f7
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,11 +13,11 @@ export const router = createRouter({
       component: MainPageView,
     },
     {
-<<<<<<< HEAD
       path: '/register',
       name: 'RegisterPage',
       component: RegisterPageView,
-=======
+    },
+    {
       path: '/newproduct',
       name: 'CreateProduct',
       component: CreateProductPageView,
@@ -29,7 +26,6 @@ export const router = createRouter({
       path: '/login',
       name: 'LoginPage',
       component: LoginPageView,
->>>>>>> 5d848de14d86f39d589fed72cef192d54c6cd5f7
     },
   ]
 });
