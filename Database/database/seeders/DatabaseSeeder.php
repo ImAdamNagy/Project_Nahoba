8ac19f666d70a69d3ccffaae42a5a2d3e59e966a
Nagy Ádám Attila <adamattila02@gmail.com>
2023-03-22 13:52:25 +0100

    ProductSeeder created, products added

--- Database/database/seeders/DatabaseSeeder.php
+++ Database/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UserSeeder::class,TypeSeeder::class]);
+        $this->call([UserSeeder::class,TypeSeeder::class,ProductSeeder::class]);
     }
 }
---
+++ Database/database/seeders/ProductSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            [
+                "seller_id" => "1",
+                "product_name" => "Wrench",
+                "product_price" => "1500",
+                "types_id" => "3",
+                "product_img" => "wrench.jpg",
+                "product_description" => "It is a long established fact that a reader 
+                will be distracted"
+            ],
+            [
+                "seller_id" => "1",
+                "product_name" => "V8 Engine",
+                "product_price" => "5000",
+                "types_id" => "1",
+                "product_img" => "engine.jpg",
+                "product_description" => "It is a long established fact that a reader 
+                will be distracted"
+            ],
+            [
+                "seller_id" => "1",
+                "product_name" => "Catalyst",
+                "product_price" => "3000",
+                "types_id" => "3",
+                "product_img" => "cata.jpg",
+                "product_description" => "It is a long established fact that a reader 
+                will be distracted"
+            ]
+        ]);
+    }
+}
