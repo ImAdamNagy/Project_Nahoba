b74ea4d874893b80fb668bbde968b37063747083
batoriandras <batori.andras@gmail.com>
2023-03-22 14:10:48 +0100

    userprofile page

---
+++ NahobaVue/nahoba/src/views/UserProfilePageView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import UserProfile from '../components/layouts/UserProfile.vue'
+
+</script>
+
+<template>
+    <div class="container-fluid">
+        <UserProfile />
+    </div>
+</template>
+<style scoped>
+</style>
