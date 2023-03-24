6b9af8c8088c9d3686684c4ec70da190862f0132
batoriandras <batori.andras@gmail.com>
2023-03-24 12:03:29 +0100

    user profile basics

--- NahobaVue/nahoba/src/components/layouts/UserProfile.vue
+++ NahobaVue/nahoba/src/components/layouts/UserProfile.vue
@@ -1,9 +1,38 @@
-<script setup>
+<script>
 </script>
 <template>
-    <div>
-
+    <header>
+        
+    </header>
+    <div class="row mx-auto my-5 rounded-3 py-4 px-3">
+        <div class="col-sm-3 ">
+            <img src="../img/pfp.webp" alt="pfp" class="img rounded-3">
+            <button class="btn btn-secondary rounded-pill mt-3 w-100">Üzeenet</button>
+        </div>
+        <div class="col-9 ">
+            <h1>Elek</h1>
+            <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
     </div>
 </template>
 <style scoped>
+header{
+    background-image: url("../img/sales.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-bottom: 2px solid white;
+    height: 100px;
+}
+img{
+    max-width: 100%;
+}
+.row{
+        background-color: #d1d1d1;   
+    }
+@media screen and (min-width: 992px) {
+    .row{
+        max-width: 75%;   
+    }
+}
 </style>
--- NahobaVue/nahoba/src/views/UserProfilePageView.vue
+++ NahobaVue/nahoba/src/views/UserProfilePageView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import UserProfile from '../components/layouts/UserProfile.vue'
-
+import NavBar from '../components/layouts/NavBar.vue';
 </script>
 
 <template>
+    <NavBar />
     <div class="container-fluid">
         <UserProfile />
     </div>
