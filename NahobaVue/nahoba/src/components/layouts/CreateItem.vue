6f601d21e3442a29072996fbcf35aeed62e8f08e
Nagy Ádám Attila <adamattila02@gmail.com>
2023-03-30 19:23:21 +0200

    CreateProduct form changed to VForm, validation is working. Form aligned verticaly. NavBar bug fixed on CreateProduct page

---
+++ NahobaVue/nahoba/src/components/layouts/CreateItem.vue
@@ -0,0 +1,106 @@
+<template>
+<div class="row mx-auto mt-5">
+        <div class="col-xl-5 text-center bg-warning" id="first">
+            <h1>Create a new product</h1>
+        </div> 
+</div>
+<div class="row mx-auto">
+        <div class="col-xl-5 text-center" id="second">
+            <div id="formdiv">
+        <VForm @submit="createProduct">
+                <label for="product_name" class="form-label">Product Name</label>
+                <Field type="text" name="product_name" id="product_name" placeholder="Enter product name" class="form-control" rules="required"/>
+                <ErrorMessage name="product_name" as="div" class="alert alert-danger m-1" />
+
+                <label for="types_id" class="form-label">Product Type</label>
+                <Field type="text" name="types_id" id="types_id" placeholder="Enter product type" class="form-control" rules="required"/>
+
+                <!-- 
+                <label for="type">Product type</label>
+                <select name="type" id="type" class="form-control">
+                    <option  v-for="item in props.data" :value="item.id">
+                        {{ item.type }}
+                    </option>
+                </select>
+                -->
+
+                <label for="product_price" class="form-label">Product Price</label>
+                <Field type="text" name="product_price" id="product_price" placeholder="Enter product type" class="form-control" rules="required"/>
+                <ErrorMessage name="product_price" as="div" class="alert alert-danger m-1" />
+
+                <label for="product_description" class="form-label">Description</label>
+                <Field type="text" name="product_description" id="product_description" placeholder="Write a short description" class="form-control" rules="required"/>
+                <ErrorMessage name="product_description" as="div" class="alert alert-danger m-1" />
+
+                <label for="loc" class="form-label">Location</label>
+                <Field type="text" name="product_location" id="product_location" placeholder="Enter product location" class="form-control" rules="required"/>
+                <ErrorMessage name="product_location" as="div" class="alert alert-danger m-1" />
+
+                <label for="img" class="form-label">Upload image</label>
+                <Field type="text" name="product_img" id="product_img" placeholder="Enter your image name" class="form-control" rules="required"/>
+
+                <button class="btn-warning btn form-control mt-3" type="submit">Create</button>
+            </VForm>
+            </div>
+        </div>
+</div>
+
+            
+</template>
+
+<script setup>
+import {Form as VForm, Field, ErrorMessage} from "vee-validate";
+import { defineProps } from "vue";
+import {router} from '@/router/index.js';
+import {http} from '@/utils/http.mjs';
+
+const props = defineProps({
+    data: Object
+});
+
+async function createProduct(newproduct){
+    const response = await http.post('createproduct', newproduct);
+    router.push({name: "MainPage"});
+}
+
+</script>
+<style scoped>
+    .row{
+        position: relative;
+        top: 5%;
+    }
+    @media screen and (max-width: 1000px) {
+        .row{
+        top: 3%;
+    }
+}
+    input{
+        margin-top: 7px;
+    }
+    #formdiv{
+        padding: 10px;
+    }
+    #second{
+        box-shadow: 10px 1px 20px -2px rgb(22, 22, 22);
+        background-color: #D9D9D9;
+        border-end-end-radius: 30px;
+        margin: auto;
+    }
+    #first{
+        box-shadow: 10px 1px 20px -2px rgb(22, 22, 22);
+        background-image: url(../img/porsche.jpg);
+        background-position: center;
+        background-size: cover;
+        padding-top: 15%;
+        border-start-start-radius: 30px;
+        margin: auto;
+
+    }
+    h1,p{
+        color: white;
+        text-align: center;
+    }
+    #title{
+        color:black;
+    }
+</style>
