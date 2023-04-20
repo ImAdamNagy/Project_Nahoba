4753d8e673d4368575272b77d1c9ce622907ba41
Nagy Ádám Attila <adamattila02@gmail.com>
2023-04-20 13:01:31 +0200

    UpdateYourProduct form now has valdiation, and types_id select got deleted, it now gets apssed manually

--- NahobaVue/nahoba/src/components/UserProfile.vue
+++ NahobaVue/nahoba/src/components/UserProfile.vue
@@ -4,37 +4,50 @@ import {reactive} from 'vue';
 import { defineProps } from "vue";
 import { useRouter } from 'vue-router';
 import {Form as VForm, Field, ErrorMessage} from "vee-validate";
+import * as yup from 'yup';
 
 const router = useRouter();
 
+const schema = yup.object(
+    {
+        product_name: yup.string('The given name is not a text!').min(5, 'The given name must be at elast 5 characters long!').required('The products name is required!'),
+        product_price: yup.number().required('You must give your product a price!'),
+        product_description: yup.string('Your description must not be a number!').max(150, 'Your items description`s length must not be greater than 150 characters!').required('You must write a hsort description of your product!'),
+        product_location: yup.string('Your location format is not correct! Try this format: `city, district`').min(8, ).max(120, 'Your location must not be greater than 120 characters!').required('You must give a location from where you are advertising your product!'),
+        product_img: yup.string().min(4).required('You must select at least one image for your product!')
+    });
 const props = defineProps({
-    data: Object,
-    types: Object
+    data: Object
 });
 const userData = reactive({
     data: {}
 });
 const itemData = reactive({
-    helper: ""
+    helper: "",
+    typesHelper: {}
 });
+
 async function user(){
     const response = await http.get('/user/' + localStorage.getItem("userid"))
         userData.data = response.data.data;
 }
 user();
-async function setId(asd){
-    itemData.helper = asd;
-    console.log(itemData.helper);
+async function setIdAndTypesId(sellerId, types){
+    itemData.helper = sellerId;
+    itemData.typesHelper = types.id;
+    console.log(props.data);
+    console.log(itemData.typesHelper);
 }
 
 async function update(updatedproduct){
     updatedproduct.seller_id = localStorage.getItem("userid");
+    updatedproduct.types_id = itemData.typesHelper;
     updatedproduct.product_enable = false;
-    console.log(updatedproduct);
     const response = await http.patch('/product/' + itemData.helper, updatedproduct,{
                 headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
         });
-    alert("Item successfully updated!")
+    alert("Your changes has been sent to the admin for validation. You may close this window.")
+    router.push({name: "UserProfile"});
 }
 
 </script>
@@ -67,39 +80,35 @@ async function update(updatedproduct){
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <VForm @submit="update">
+                                    <VForm @submit="update" :validation-schema="schema">
                                         <div class="mb-3">
                                             <label for="product_name" class="form-label">Product name</label>
                                             <Field type="text" name="product_name" id="product_name" class="form-control" :value="`${item.product_name}`" />
+                                            <ErrorMessage name="product_name" as="div" class="alert alert-danger m-1" />
                                         </div>
-                                        <div class="mb-3">
-                                            <label for="types_id">Product type</label>
-                
-                                            <Field name="types_id" id="types_id" class="form-control" as="select">
-                                                <option value="" disabled selected hidden>Choose your products type</option>
-                                                <option  v-for="item in props.types" :value="item.id">
-                                                    {{ item.type }}
-                                                </option>
-                                            </Field>
-                                        </div>
+                                        
                                         <div class="mb-3">
                                             <label for="product_price" class="form-label">Product price</label>
                                             <Field type="number" name="product_price" id="product_price" class="form-control" :value="`${item.product_price}`"/>
+                                            <ErrorMessage name="product_price" as="div" class="alert alert-danger m-1" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="product_description" class="form-label">Product descripton</label>
                                             <Field type="text" as="textarea" name="product_description" id="product_description" class="form-control" :value="`${item.product_description}`"/>
+                                            <ErrorMessage name="product_description" as="div" class="alert alert-danger m-1" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="product_location" class="form-label">Product location</label>
                                             <Field type="text" as="textarea" name="product_location" id="product_location" class="form-control" :value="`${item.product_location}`"/>
+                                            <ErrorMessage name="product_location" as="div" class="alert alert-danger m-1" />
                                         </div>
                                         <div class="mb-3">
                                             <label for="product_img" class="form-label">Upload image</label>
                                             <br>
                                             <Field type="text" id="product_img" name="product_img" ref="file" :value="`${item.product_img}`"/>
+                                            <ErrorMessage name="product_img" as="div" class="alert alert-danger m-1" />
                                         </div>
-                                        <button type="submit" class="btn btn-primary" @click="setId(item.id)">Update</button>
+                                        <button type="submit" class="btn btn-primary" @click="setIdAndTypesId(item.id, item.type)">Update</button>
                                     </VForm>
                                 </div>
                                 <div class="modal-footer">
