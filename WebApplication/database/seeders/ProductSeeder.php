b4ab418c15503bdf60d34d2faa73249e93c4aa5e
ImAdamNagy <adamattila02@gmail.com>
2023-05-12 16:09:05 +0200

    Product seeder and type seeder edited

--- WebApplication/database/seeders/ProductSeeder.php
+++ WebApplication/database/seeders/ProductSeeder.php
@@ -17,87 +17,113 @@ class ProductSeeder extends Seeder
     {
         DB::table('products')->insert([
             [
-                "seller_id" => 1,
-                "product_name" => "Wrench",
-                "product_price" => 1500,
-                "types_id" => 3,
+                "seller_id" => 2,
+                "product_name" => "Steering wheel",
+                "product_price" => 8000,
+                "types_id" => 2,
                 "car_typeId" => 1,
-                "product_img" => "wrench.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
+                "product_img" => "steer.jpg",
+                "product_description" => "Used steering wheel for audi",
                 "product_location" => "Budapest 18.",
                 "product_enable" => true
             ],
             [
-                "seller_id" => 1,
-                "product_name" => "V8 Engine",
-                "product_price" => 5000,
-                "types_id" => 1,
-                "car_typeId" => 1,
-                "product_img" => "engine.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
+                "seller_id" => 2,
+                "product_name" => "Clutch",
+                "product_price" => 4000,
+                "types_id" => 11,
+                "car_typeId" => 9,
+                "product_img" => "clutch.jpg",
+                "product_description" => "Clutch top",
                 "product_location" => "Budapest 10.",
                 "product_enable" => true
             ],
             [
-                "seller_id" => 1,
-                "product_name" => "Catalyst",
-                "product_price" => 3000,
-                "types_id" => 3,
+                "seller_id" => 2,
+                "product_name" => "Front light",
+                "product_price" => 11000,
+                "types_id" => 8,
+                "car_typeId" => 15,
+                "product_img" => "opellight.jpg",
+                "product_description" => "Front left light",
+                "product_location" => "Budapest 20.",
+                "product_enable" => true
+            ],
+            [
+                "seller_id" => 4,
+                "product_name" => "Front bumper",
+                "product_price" => 6000,
+                "types_id" => 16,
                 "car_typeId" => 1,
-                "product_img" => "cata.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
+                "product_img" => "opelcoolingfront.jpg",
+                "product_description" => "Brand new front bumper",
                 "product_location" => "Budapest 20.",
                 "product_enable" => true
             ],
             [
-                "seller_id" => 1,
-                "product_name" => "Catalyst",
-                "product_price" => 3500,
-                "types_id" => 3,
-                "car_typeId" => 1,
-                "product_img" => "cata.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
-                "product_location" => "Budapest 20.",
+                "seller_id" => 4,
+                "product_name" => "Rear light",
+                "product_price" => 7300,
+                "types_id" => 8,
+                "car_typeId" => 4,
+                "product_img" => "rearlight.jpg",
+                "product_description" => "Rear light for sedan models. It is used but functioning perfectly",
+                "product_location" => "Budapest 6.",
                 "product_enable" => true
             ],
             [
-                "seller_id" => 1,
-                "product_name" => "Catalyst",
-                "product_price" => 1500,
-                "types_id" => 3,
-                "car_typeId" => 2,
-                "product_img" => "cata.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
-                "product_location" => "Budapest 20.",
-                "product_enable" => true
-            ],
-            [
-                "seller_id" => 1,
-                "product_name" => "Catalyst",
+                "seller_id" => 4,
+                "product_name" => "Trunk lock",
                 "product_price" => 500,
                 "types_id" => 3,
                 "car_typeId" => 2,
-                "product_img" => "cata.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
-                "product_location" => "Budapest 20.",
+                "product_img" => "backpack.jpg",
+                "product_description" => "Used lock for trunks. Still capable of locking",
+                "product_location" => "Budapest 19.",
                 "product_enable" => true
             ],
             [
-                "seller_id" => 1,
-                "product_name" => "Catalyst",
+                "seller_id" => 3,
+                "product_name" => "Two reflector front light",
                 "product_price" => 7000,
                 "types_id" => 3,
                 "car_typeId" => 2,
                 "product_img" => "cata.jpg",
-                "product_description" => "It is a long established fact that a reader 
-                will be distracted",
-                "product_location" => "Budapest 20.",
+                "product_description" => "Brand new frontlight with stronger capability of lighting.",
+                "product_location" => "Budapest 13.",
+                "product_enable" => true
+            ],
+            [
+                "seller_id" => 3,
+                "product_name" => "Steer switches",
+                "product_price" => 16000,
+                "types_id" => 2,
+                "car_typeId" => 20,
+                "product_img" => "steerswitch.jpg",
+                "product_description" => "A pair of steer switches",
+                "product_location" => "Budapest 12.",
+                "product_enable" => false
+            ],
+            [
+                "seller_id" => 3,
+                "product_name" => "Rear wing",
+                "product_price" => 18000,
+                "types_id" => 5,
+                "car_typeId" => 17,
+                "product_img" => "wing.jpg",
+                "product_description" => "Medium sized rear wing",
+                "product_location" => "Budapest 15.",
+                "product_enable" => false
+            ],
+            [
+                "seller_id" => 3,
+                "product_name" => "Mirror",
+                "product_price" => 9000,
+                "types_id" => 16,
+                "car_typeId" => 10,
+                "product_img" => "leftmirro.jpg",
+                "product_description" => "Factory new left mirror",
+                "product_location" => "Budapest 13.",
                 "product_enable" => false
             ]
         ]);
